816334260e78da5da462b556a4275dbe9d3fe344
JonathanQ-Maker <jonathanqfxw@gmail.com>
2022-09-02 21:44:49 -0700

    Added useless health bar

---
+++ Assets/Scripts/Entity/Prop/CampFire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RPG
+{
+    public class CampFire : PropEntity
+    {
+
+        public float hoverScale = 1.1f;
+
+        public override void OnHover(CharacterEntity interactee)
+        {
+            transform.localScale *= hoverScale;
+        }
+
+        public override void OnUnhover(CharacterEntity interactee)
+        {
+            transform.localScale /= hoverScale;
+        }
+    }
+}
